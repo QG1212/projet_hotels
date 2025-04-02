e54cb744ec04b7c11468179d237969bb20c4fa05
Mathis Letellier <mathis.letellier@isen-ouest.yncrea.fr>
2025-04-02 11:06:56 +0200

    Profil client back end put
    Ajout d'un fonction updateclient qui modifie le client et le form modifie désormais le client quand celui ci appuie sur mise à jour

--- Client/Profil_Client/Profil.php
+++ Client/Profil_Client/Profil.php
@@ -6,7 +6,10 @@ if (!isset($_SESSION['user_id'])) {
     header('Location: ../login/login.php');
 }
 $pdo=dbConnect();
+
+if(!empty($_POST["prenom"]) and !empty($_POST["nom"]) and !empty($_POST["email"]) and !empty($_POST["tel"])){
+    Client_Model::UpdateClient($pdo,$_SESSION["user_id"],$_POST["nom"],$_POST["prenom"],$_POST["tel"],$_POST["email"]);
+}
 $client = Client_model::getClient($_SESSION['user_id'],$pdo);
 $prenom = $client['prenom'];
-
 require("Profil_vue.php");
--- Serveur/model/Client_model.php
+++ Serveur/model/Client_model.php
@@ -63,5 +63,31 @@ class Client_Model{
             die("Erreur lors de l'inscription : " . $e->getMessage());
         }
     }
+
+    /**
+     * Modifie les datas d'un client
+     *
+     * @param $pdo DatabaseObject
+     * @param $id_client int id du client dont on modifie ses datas
+     * @param $nom string nouveau nom
+     * @param $prenom string nouveau prenom
+     * @param $tel string nouveau numero de tel
+     * @param $mail string nouveau mail
+     * @return void
+     */
+    static function UpdateClient($pdo, $id_client, $nom, $prenom, $tel,$email){
+        $stmt=$pdo->prepare("UPDATE Client 
+                                SET prenom = :prenom, 
+                                    nom = :nom, 
+                                    email = :mail, 
+                                    tel = :tel 
+                                WHERE id_client = :id_client;");
+        $stmt->bindParam(':prenom', $prenom);
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':mail', $email);
+        $stmt->bindParam(':tel', $tel);
+        $stmt->bindParam(':id_client', $id_client);
+        $stmt->execute();
+    }
     }
 ?>
