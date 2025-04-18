44e4a5fd1521ec1fd696a8277a45112283bfd8e4
Etes-Hyene <etienne.decamps@isen-ouest.yncrea.fr>
2025-04-18 11:47:53 +0200

    début vue et controlleur conso admin

--- Admin/Admin_Consommation/Consommation_Vue_Admin.php
+++ Admin/Admin_Consommation/Consommation_Vue_Admin.php
@@ -1,1 +1,14 @@
 <?php
+
+if ($hotel==""){
+    echo "ERROR: hotel was not found";
+}
+else{
+    echo "Prix des consommation de l'hotel de".$hotel.":";
+    echo"<br><form>";
+    foreach ($consoList as $conso ){
+        echo $conso["denomination"]."<br>";
+        echo "<input type='number' value=".$conso["prix"]."></input>"." €";
+    }
+    echo "<button type='submit'>Valider les modifications</button> </form>";
+}
--- Admin/Admin_Consommation/Consommation_admin.php
+++ Admin/Admin_Consommation/Consommation_admin.php
@@ -2,12 +2,27 @@
 require "../../Serveur/model/Consomation_model.php";
 
 $db=dbConnect();
-$consoList= Consomation::get_all_consomation();
 
+switch ($_SESSION['employe_id']){
+    case 5:
+    $hotel= "Caen";
+        break;
+    case 7:
+    $hotel= "Brest";
+        break;
+    case 8:
+    $hotel= "Paris";
+        break;
+    case 6:
+    $hotel= "Nantes";
+        break;
 
+    default:
+        $hotel="";
+            break;
 
-
-
+}
+$consoList= Consomation::get_all_consomation($db,$hotel);
 
 
 require "Consommation_Vue_Admin.php";
