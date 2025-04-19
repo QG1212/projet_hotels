7fe139eafba164a143f4a1fd3f4aac1a54fed17f
Mathis Letellier <mathis.letellier@isen-ouest.yncrea.fr>
2025-04-19 20:07:12 +0200

    tkt

--- Admin/formulaire_chambre/eddit_room.php
+++ Admin/formulaire_chambre/eddit_room.php
@@ -1,10 +1,4 @@
 <?php
-require_once "../../Serveur/model/Chambre.php";
-
-
-$id_chambre = Chambre::GetSelectChambre();
-$nom_categorie = Chambre::GetSelectCategorie();
-$id_classe = Chambre::GetSelectClass();
 
 
 require_once("eddit_room_vue.php");
