76df5a6bf7c2712274ac97f148484cc615f52c34
tomcabanau <tom.cabanau@gmail.com>
2025-04-22 13:56:34 +0200

    baba

--- Client/Login/login.php
+++ Client/Login/login.php
@@ -2,27 +2,12 @@
 require_once("../../Serveur/database/constants.php");
 require_once("../../Serveur/model/Client_model.php");
 require_once("../../Serveur/model/login_model.php");
-//connexion bdd
-/*
-$host = 'localhost';
-$dbname = 'projet_hotel';
-$user = 'dev';
-$password = 'isen';
-try {
-    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion à la base de données : " . $e->getMessage());
-}
-*/
+
 session_start();
 if(!empty($_POST["deconnection"])){
     session_destroy();
-    //echo "<h1>destroyyyyyyyyyyyy</h1>".$_SESSION["user_id"];
 }
 $pdo=dbConnect();
-//gestion formulaire
-//if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     //pour connexion
     if (isset($_POST['action']) && $_POST['action'] === 'login') {
